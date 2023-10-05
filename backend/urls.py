39fea18ddf03893db2f4d434445d693ddf3f70ec
Igwesi Emmanuel Ugochukwu <emmaigwesi@gmail.com>
2023-10-05 14:06:26 +0100

    Changed DEBUG

--- backend/urls.py
+++ backend/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+settings.DEBUG = False
