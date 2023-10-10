285f81e7adee9d87f24edceae445e1aaf45b26d1
Igwesi Emmanuel Ugochukwu <emmaigwesi@gmail.com>
2023-10-10 23:27:46 +0100

    Added Services Workers

---
+++ plugins/static/js/service-worker.js
@@ -0,0 +1,37 @@
+// Install and activate the service worker
+self.addEventListener('install', (event) => {
+     event.waitUntil(
+       caches.open('app-cache').then((cache) => {
+         return cache.addAll([
+           '/',
+           '/homepage.html',
+           '/static/css/styles.css',
+           '/static/js/script.js',
+           '/static/images/icon.png',
+         ]);
+       })
+     );
+  });
+   
+   self.addEventListener('activate', (event) => {
+     event.waitUntil(
+       caches.keys().then((cacheNames) => {
+         return Promise.all(
+           cacheNames.filter((cacheName) => {
+            return cacheName.startsWith('app-cache') && cacheName !== 'app-cache';
+            }).map((cacheName) => {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
+  
+  // Fetch resources from cache or network
+  self.addEventListener('fetch', (event) => {
+     event.respondWith(
+       caches.match(event.request).then((response) => {
+         return response || fetch(event.request);
+       })
+     );
+  });
