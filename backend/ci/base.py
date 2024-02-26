b779ac625b502ad56a7ba238388b105a39218d88
Igwesi Emmanuel Ugochukwu <emmaigwesi@gmail.com>
2024-02-26 10:18:13 +0100

    Updated

--- backend/ci/base.py
+++ backend/ci/base.py
@@ -60,6 +60,7 @@ HEALTH_CHECK['DISK_USAGE_MAX'] = 5 * (1 << 30)   # 5GB in bytes
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -98,16 +99,6 @@ EMAIL_USE_TLS       = True
 EMAIL_USE_SSL       = False  # Set to False for TLS
 EMAIL_HOST_USER     = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
